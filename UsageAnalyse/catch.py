66832a1c492beed7796273186d5a30e736b426b4
BYOUINZAKA <2606675531@qq.com>
2020-02-10 08:21:55 +0800

    beta-0.0.1 200210

---
+++ UsageAnalyse/catch.py
@@ -0,0 +1,41 @@
+import re
+
+from pandas import DataFrame
+
+
+def get_date(url: str):
+    if url.find('https://www.smogon.com/stats/') == -1:
+        return None
+    else:
+        i = len(str('https://www.smogon.com/stats/'))
+        _url = url[i:]
+        j = (_url).find('/')
+        return _url[:j]
+
+
+def get_mode(url: str):
+    if url.rfind('.txt') == -1:
+        return None
+    else:
+        pa = re.compile(r'.+[/](\S+?)[-]')
+        return pa.findall(url)
+
+
+def get_score(url: str):
+    if url.rfind('.txt') == -1:
+        return None
+    else:
+        pa = re.compile(r'[-](\d+?)[.]')
+        return pa.findall(url)
+
+
+def split_data(text: str):
+    lines = re.split(r'[\n]', text)
+    pa = re.compile(
+        r'^\s[|]\s*(\d+)\s*[|]\s*(.+?)\s*[|]\s*(\S+[%])\s*[|].+[|]\s$')
+    list = []
+    for l in lines:
+        result = pa.findall(l)
+        if len(result) != 0:
+            list.append(result[0])
+    return list
